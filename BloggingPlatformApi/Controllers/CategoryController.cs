0749b3bb9b7ab29d491960a707df54d117e07a4b
furkant <fturgut1126@gmail.com>
2024-09-21 18:34:31 +0300

    Add Category Controller with HTTP GET methods

---
+++ BloggingPlatformApi/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using BloggingPlatformApi.Dto_s;
+using BloggingPlatformApi.Interface;
+using BloggingPlatformApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggingPlatformApi.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(IMapper mapper, ICategoryRepository categoryRepository)
+        {
+            this._mapper = mapper;
+            this._categoryRepository = categoryRepository;
+        }
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(ICollection<CategoryDto>))]
+        public ActionResult GetCategories()
+        {
+            var categories = _mapper.Map<List<CategoryDto>>(_categoryRepository.GetCategories());
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(categories);
+        }
+
+        [HttpGet("{CategoryId}")]
+        [ProducesResponseType(200, Type = typeof(CategoryDto))]
+        [ProducesResponseType(400)]
+        public ActionResult GetCategory(int CategoryId)
+        {
+            if (!_categoryRepository.CategoryExist(CategoryId))
+            {
+                return NotFound();
+            }
+
+            var category = _mapper.Map<CategoryDto>(_categoryRepository.GetCategory(CategoryId));
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(category);
+        }
+
+    }
+}
