d4aa3c85a2ae89f41084fb08686b6a6638e6b840
furkant <fturgut1126@gmail.com>
2024-09-25 03:52:46 +0300

    Create Interface and Repository for Tag and implement Read methods

---
+++ BloggingPlatformApi/Interface/ITagRepository.cs
@@ -0,0 +1,13 @@
+﻿using BloggingPlatformApi.Models;
+
+namespace BloggingPlatformApi.Interface
+{
+    public interface ITagRepository
+    {
+        ICollection<Tag> GetTags();
+        Tag GetTag(int TagId);
+        bool TagExist (int TagId);
+        ICollection<Article> GetArticleByTag(int TagId);
+
+    }
+}
---
+++ BloggingPlatformApi/Repository/TagRepository.cs
@@ -0,0 +1,36 @@
+﻿using BloggingPlatformApi.Data;
+using BloggingPlatformApi.Interface;
+using BloggingPlatformApi.Models;
+
+namespace BloggingPlatformApi.Repository
+{
+    public class TagRepository : ITagRepository
+    {
+        private readonly DataContext _context;
+
+        public TagRepository(DataContext context)
+        {
+            this._context = context;
+        }
+
+        public ICollection<Article> GetArticleByTag(int TagId)
+        {
+            return _context.ArticleTags.Where(t=> t.TagId == TagId).Select(t=>t.Article).ToList();
+        }
+
+        public Tag GetTag(int TagId)
+        {
+            return _context.Tags.FirstOrDefault(t => t.Id == TagId);
+        }
+
+        public ICollection<Tag> GetTags()
+        {
+            return _context.Tags.ToList();
+        }
+
+        public bool TagExist(int TagId)
+        {
+            return _context.Tags.Any(t => t.Id == TagId);
+        }
+    }
+}
